GNOME/nautilus

commit 5a47a484e45218e83202c508b421b1a2707af270

link: fix code to parse additional description for links
For some reason, this was ifdeffed out.

src/nautilus-icon-view-container.c
@@ -311,7 +311,6 @@ nautilus_icon_view_container_get_icon_text (NautilusIconContainer *container,
 					    gboolean               include_invisible)
 {
 	char *actual_uri;
-	gchar *description;
 	GQuark *attributes;
 	char *text_array[4];
 	int i, j, num_attributes;
@@ -359,10 +358,8 @@ nautilus_icon_view_container_get_icon_text (NautilusIconContainer *container,
  		*additional_text = NULL;
 		if (nautilus_file_is_local (file)) {
 			actual_uri = nautilus_file_get_uri (file);
-			description = nautilus_link_local_get_additional_text (actual_uri);
-			if (description)
-				*additional_text = g_strdup_printf (" \n%s\n ", description);
-			g_free (description);
+			*additional_text = nautilus_link_local_get_additional_text (actual_uri);
+
 			g_free (actual_uri);
 		}
 		/* Don't show the normal extra information for desktop files, it doesn't

libnautilus-private/nautilus-link.c
@@ -351,14 +351,8 @@ nautilus_link_local_get_text (const char *path)
 }
 
 char *
-nautilus_link_local_get_additional_text (const char *path)
+nautilus_link_local_get_additional_text (const char *uri)
 {
-	/* The comment field of current .desktop files is often bad.
-	 * It just contains a copy of the name. This is probably because the
-	 * panel shows the comment field as a tooltip.
-	 */
-	return NULL;
-#ifdef THIS_IS_NOT_USED_RIGHT_NOW
 	char *type;
 	char *retval;
 
@@ -366,20 +360,19 @@ nautilus_link_local_get_additional_text (const char *path)
 		return NULL;
 	}
 
-	type = slurp_key_string (path, "Type", FALSE);
+	type = slurp_key_string (uri, "Type", FALSE);
 	retval = NULL;
 	if (type == NULL) {
 		return NULL;
 	}
 
 	if (strcmp (type, "Application") == 0) {
-		retval = slurp_key_string (path, "Comment", TRUE);
+		retval = slurp_key_string (uri, "Comment", TRUE);
 	}
 	
 	g_free (type);
 
 	return retval;
-#endif
 }
 
 static char *

linuxmint/nemo

commit 0ad87aaf34da71d3bf328074533c573331ebb0ea

icon-container: don't show comment field for desktop files
This fixes a regression introduced in commit
5a47a484e45218e83202c508b421b1a2707af270

https://bugzilla.gnome.org/show_bug.cgi?id=673316

src/nautilus-icon-view-container.c
@@ -310,7 +310,6 @@ nautilus_icon_view_container_get_icon_text (NautilusIconContainer *container,
 					    char                 **additional_text,
 					    gboolean               include_invisible)
 {
-	char *actual_uri;
 	GQuark *attributes;
 	char *text_array[4];
 	int i, j, num_attributes;
@@ -345,25 +344,11 @@ nautilus_icon_view_container_get_icon_text (NautilusIconContainer *container,
 		return;
 	}
 
-	if (NAUTILUS_IS_DESKTOP_ICON_FILE (file)) {
-		/* Don't show the normal extra information for desktop icons, it doesn't
-		 * make sense. */
+	if (NAUTILUS_IS_DESKTOP_ICON_FILE (file) ||
+	    nautilus_file_is_nautilus_link (file)) {
+		/* Don't show the normal extra information for desktop icons,
+		 * or desktop files, it doesn't make sense. */
  		*additional_text = NULL;
-		return;
-	}
-	
-	/* Handle link files specially. */
-	if (nautilus_file_is_nautilus_link (file)) {
-		/* FIXME bugzilla.gnome.org 42531: Does sync. I/O and works only locally. */
- 		*additional_text = NULL;
-		if (nautilus_file_is_local (file)) {
-			actual_uri = nautilus_file_get_uri (file);
-			*additional_text = nautilus_link_local_get_additional_text (actual_uri);
-
-			g_free (actual_uri);
-		}
-		/* Don't show the normal extra information for desktop files, it doesn't
-		 * make sense. */
 		return;
 	}
 