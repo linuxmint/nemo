2d8d3def8c1abc2a5fc83d0277d2765331662d08
Cosimo Cecchi <cosimoc@gnome.org>
2012-04-11 12:33:10 -0400

    thumbnails: remove unused code

--- libnautilus-private/nautilus-thumbnails.h
+++ libnautilus-private/nautilus-thumbnails.h
@@ -28,21 +28,6 @@
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <libnautilus-private/nautilus-file.h>
 
-typedef struct NautilusThumbnailAsyncLoadHandle NautilusThumbnailAsyncLoadHandle;
-
-typedef void (* NautilusThumbnailAsyncLoadFunc) (NautilusThumbnailAsyncLoadHandle *handle,
-						 const char *path,
-						 GdkPixbuf  *pixbuf,
-						 double      scale_x,
-						 double      scale_y,
-						 gpointer    user_data);
-
-
-#define NAUTILUS_THUMBNAIL_FRAME_LEFT 3
-#define NAUTILUS_THUMBNAIL_FRAME_TOP 3
-#define NAUTILUS_THUMBNAIL_FRAME_RIGHT 3
-#define NAUTILUS_THUMBNAIL_FRAME_BOTTOM 3
-
 /* Returns NULL if there's no thumbnail yet. */
 void       nautilus_create_thumbnail                (NautilusFile *file);
 gboolean   nautilus_can_thumbnail                   (NautilusFile *file);
@@ -50,30 +35,9 @@ gboolean   nautilus_can_thumbnail_internally        (NautilusFile *file);
 gboolean   nautilus_thumbnail_is_mimetype_limited_by_size
 						    (const char *mime_type);
 void       nautilus_thumbnail_frame_image           (GdkPixbuf **pixbuf);
-GdkPixbuf *nautilus_thumbnail_unframe_image         (GdkPixbuf  *pixbuf);
-GdkPixbuf *nautilus_thumbnail_load_image            (const char *path,
-						     guint       base_size,
-						     guint       nominal_size,
-						     gboolean    force_nominal,
-						     double     *scale_x_out,
-						     double     *scale_y_out);
-NautilusThumbnailAsyncLoadHandle *
-	   nautilus_thumbnail_load_image_async	    (const char *path,
-						     guint       base_size,
-						     guint       nominal_size,
-						     gboolean    force_nominal,
-						     NautilusThumbnailAsyncLoadFunc load_func,
-						     gpointer    load_func_user_data);
-void       nautilus_thumbnail_load_image_cancel     (NautilusThumbnailAsyncLoadHandle *handle);
-void       nautilus_update_thumbnail_file_copied    (const char   *source_file_uri,
-						     const char   *destination_file_uri);
-void       nautilus_update_thumbnail_file_renamed   (const char   *source_file_uri,
-						     const char   *destination_file_uri);
-void       nautilus_remove_thumbnail_for_file       (const char   *file_uri);
 
 /* Queue handling: */
 void       nautilus_thumbnail_remove_from_queue     (const char   *file_uri);
-void       nautilus_thumbnail_remove_all_from_queue (void);
 void       nautilus_thumbnail_prioritize            (const char   *file_uri);
 
 
