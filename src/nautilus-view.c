e0b13d7fb2205671653b4c9aa1b057ec11e9a8b8
William Jon McCann <jmccann@redhat.com>
2012-07-02 12:04:04 -0400

    Don't show open with for folders
    The only thing that handles this by default is nautilus. The
    same effect is better done through select all in a folder.

--- src/nautilus-view.c
+++ src/nautilus-view.c
@@ -4575,9 +4575,8 @@ reset_open_with_menu (NautilusView *view, GList *selection)
 
 		file = NAUTILUS_FILE (node->data);
 
-		other_applications_visible &= ((!nautilus_mime_file_opens_in_view (file) &&
-						!nautilus_file_is_nautilus_link (file)) ||
-					       nautilus_file_is_directory (file));
+		other_applications_visible &= (!nautilus_mime_file_opens_in_view (file) &&
+					       !nautilus_file_is_nautilus_link (file));
 	}
 
 	default_app = NULL;
