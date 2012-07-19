a3df3d73876137ea69df6a00b5539e46e605f2a6
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2012-07-19 14:18:20 +0200

    Fix bookmarks handling

--- src/nemo-places-sidebar.c
+++ src/nemo-places-sidebar.c
@@ -1108,12 +1108,11 @@ compute_drop_position (GtkTreeView *tree_view,
 			    PLACES_SIDEBAR_COLUMN_SECTION_TYPE, &section_type,
 			    -1);
 
-	if (section_type != SECTION_BOOKMARKS &&
-	    place_type == PLACES_HEADING) {
+	if (place_type == PLACES_HEADING && section_type != SECTION_BOOKMARKS) {
 		/* never drop on headings, but special case the bookmarks heading,
-		 * so we can drop bookmarks in between it and the first item when
-		 * reordering.
+		 * so we can drop bookmarks in between it and the first item.
 		 */
+
 		gtk_tree_path_free (*path);
 		*path = NULL;
 		
@@ -1124,18 +1123,25 @@ compute_drop_position (GtkTreeView *tree_view,
 	    sidebar->drag_data_received &&
 	    sidebar->drag_data_info == GTK_TREE_MODEL_ROW) {
 		/* don't allow dropping bookmarks into non-bookmark areas */
+
 		gtk_tree_path_free (*path);
 		*path = NULL;
 
 		return FALSE;
 	}
 
-	if (sidebar->drag_data_received &&
-	    sidebar->drag_data_info == GTK_TREE_MODEL_ROW) {
-		/* bookmark rows can only be reordered */
+	if (section_type == SECTION_BOOKMARKS) {
 		*pos = GTK_TREE_VIEW_DROP_AFTER;
 	} else {
+		/* non-bookmark shortcuts can only be dragged into */
 		*pos = GTK_TREE_VIEW_DROP_INTO_OR_BEFORE;
+	}
+
+	if (*pos != GTK_TREE_VIEW_DROP_BEFORE &&
+	    sidebar->drag_data_received &&
+	    sidebar->drag_data_info == GTK_TREE_MODEL_ROW) {
+		/* bookmark rows are never dragged into other bookmark rows */
+		*pos = GTK_TREE_VIEW_DROP_AFTER;
 	}
 
 	return TRUE;
@@ -1174,6 +1180,36 @@ free_drag_data (NemoPlacesSidebar *sidebar)
 }
 
 static gboolean
+can_accept_file_as_bookmark (NemoFile *file)
+{
+	return (nemo_file_is_directory (file) &&
+		!is_built_in_bookmark (file));
+}
+
+static gboolean
+can_accept_items_as_bookmarks (const GList *items)
+{
+	int max;
+	char *uri;
+	NemoFile *file;
+
+	/* Iterate through selection checking if item will get accepted as a bookmark.
+	 * If more than 100 items selected, return an over-optimistic result.
+	 */
+	for (max = 100; items != NULL && max >= 0; items = items->next, max--) {
+		uri = ((NemoDragSelectionItem *)items->data)->uri;
+		file = nemo_file_get_by_uri (uri);
+		if (!can_accept_file_as_bookmark (file)) {
+			nemo_file_unref (file);
+			return FALSE;
+		}
+		nemo_file_unref (file);
+	}
+	
+	return TRUE;
+}
+
+static gboolean
 drag_motion_callback (GtkTreeView *tree_view,
 		      GdkDragContext *context,
 		      int x,
@@ -1201,10 +1237,13 @@ drag_motion_callback (GtkTreeView *tree_view,
 		goto out;
 	}
 
-	if (pos == GTK_TREE_VIEW_DROP_AFTER ) {
+	if (pos == GTK_TREE_VIEW_DROP_BEFORE ||
+	    pos == GTK_TREE_VIEW_DROP_AFTER ) {
 		if (sidebar->drag_data_received &&
 		    sidebar->drag_data_info == GTK_TREE_MODEL_ROW) {
 			action = GDK_ACTION_MOVE;
+		} else if (can_accept_items_as_bookmarks (sidebar->drag_list)) {
+			action = GDK_ACTION_COPY;
 		} else {
 			action = 0;
 		}
@@ -1252,8 +1291,52 @@ drag_leave_callback (GtkTreeView *tree_view,
 		     NemoPlacesSidebar *sidebar)
 {
 	free_drag_data (sidebar);
-	gtk_tree_view_set_drag_dest_row (tree_view, NULL, 0);
+	gtk_tree_view_set_drag_dest_row (tree_view, NULL, GTK_TREE_VIEW_DROP_BEFORE);
 	g_signal_stop_emission_by_name (tree_view, "drag-leave");
+}
+
+/* Parses a "text/uri-list" string and inserts its URIs as bookmarks */
+static void
+bookmarks_drop_uris (NemoPlacesSidebar *sidebar,
+		     GtkSelectionData      *selection_data,
+		     int                    position)
+{
+	NemoBookmark *bookmark;
+	NemoFile *file;
+	char *uri;
+	char **uris;
+	int i;
+	GFile *location;
+	
+	uris = gtk_selection_data_get_uris (selection_data);
+	if (!uris)
+		return;
+	
+	for (i = 0; uris[i]; i++) {
+		uri = uris[i];
+		file = nemo_file_get_by_uri (uri);
+
+		if (!can_accept_file_as_bookmark (file)) {
+			nemo_file_unref (file);
+			continue;
+		}
+
+		uri = nemo_file_get_drop_target_uri (file);
+		location = g_file_new_for_uri (uri);
+		nemo_file_unref (file);
+
+		bookmark = nemo_bookmark_new (location, NULL, NULL);
+
+		if (!nemo_bookmark_list_contains (sidebar->bookmarks, bookmark)) {
+			nemo_bookmark_list_insert_item (sidebar->bookmarks, bookmark, position++);
+		}
+
+		g_object_unref (location);
+		g_object_unref (bookmark);
+		g_free (uri);
+	}
+
+	g_strfreev (uris);
 }
 
 static GList *
@@ -1386,7 +1469,8 @@ drag_data_received_callback (GtkWidget *widget,
 
 	success = FALSE;
 
-	if (tree_pos == GTK_TREE_VIEW_DROP_AFTER) {
+	if (tree_pos == GTK_TREE_VIEW_DROP_BEFORE ||
+	    tree_pos == GTK_TREE_VIEW_DROP_AFTER) {
 		model = gtk_tree_view_get_model (tree_view);
 
 		if (!gtk_tree_model_get_iter (model, &iter, tree_path)) {
@@ -1409,6 +1493,10 @@ drag_data_received_callback (GtkWidget *widget,
 		}
 
 		switch (info) {
+		case TEXT_URI_LIST:
+			bookmarks_drop_uris (sidebar, selection_data, position);
+			success = TRUE;
+			break;
 		case GTK_TREE_MODEL_ROW:
 			reorder_bookmarks (sidebar, position);
 			success = TRUE;
