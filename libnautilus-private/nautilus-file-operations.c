bcc217bd8d9273f3dd15a1bc64c829bd174be94e
William Jon McCann <jmccann@redhat.com>
2012-08-22 12:14:23 -0400

    Make getting extension more robust
    Ensure the basename and extension have at least one char each.

--- libnautilus-private/nautilus-file-operations.c
+++ libnautilus-private/nautilus-file-operations.c
@@ -543,7 +543,7 @@ parse_previous_duplicate_name (const char *name,
 
 	g_assert (name[0] != '\0');
 
-	*suffix = eel_filename_get_extension_offset (name + 1);
+	*suffix = eel_filename_get_extension_offset (name);
 
 	if (*suffix == NULL || (*suffix)[1] == '\0') {
 		/* no suffix */
