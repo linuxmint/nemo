d6646b4cae346383c5120ee60baf1cf5d113449f
William Jon McCann <jmccann@redhat.com>
2012-08-30 15:46:32 -0400

    Don't allow multiline filenames
    https://bugzilla.gnome.org/show_bug.cgi?id=683059

--- src/nautilus-list-view.c
+++ src/nautilus-list-view.c
@@ -1507,6 +1507,7 @@ create_and_set_up_tree_view (NautilusListView *view)
 			view->details->file_name_cell = (GtkCellRendererText *)cell;
 			g_object_set (cell,
 				      "ellipsize", PANGO_ELLIPSIZE_END,
+				      "single-paragraph-mode", TRUE,
 				      "xpad", 5,
 				      NULL);
 
