da53f732b5fc35247eedd2357f731f2e26e213e5
Cosimo Cecchi <cosimoc@gnome.org>
2013-01-03 18:05:44 +0100

    eel: remove unused code

--- eel/eel-accessibility.c
+++ eel/eel-accessibility.c
@@ -42,46 +42,6 @@ eel_accessibility_set_up_label_widget_relation (GtkWidget *label, GtkWidget *wid
 	atk_object_add_relationship (atk_widget, ATK_RELATION_LABELLED_BY, atk_label);
 }
 
-GType
-eel_accessibility_create_accessible_gtype (const char *type_name,
-					   GtkWidget *widget,
-					   GClassInitFunc class_init)
-{
-	GType atk_type, parent_atk_type;
-	GTypeQuery query;
-	AtkObject *parent_atk;
-	GtkWidgetClass *parent_class, *klass;
-
-	if ((atk_type = g_type_from_name (type_name))) {
-		return atk_type;
-	}
-
-	klass = GTK_WIDGET_CLASS (G_OBJECT_GET_CLASS (widget));
-	parent_class = klass;
-
-	while (klass->get_accessible == parent_class->get_accessible) {
-		parent_class = g_type_class_peek_parent (parent_class);
-	}
-
-	parent_atk = parent_class->get_accessible (widget);
-	parent_atk_type = G_TYPE_FROM_INSTANCE (parent_atk);
-
-	if (!parent_atk_type) {
-		return G_TYPE_INVALID;
-	}
-
-	/* Figure out the size of the class and instance 
-	 * we are deriving from
-	 */
-	g_type_query (parent_atk_type, &query);
-
-	/* Register the type */
-	return g_type_register_static_simple (parent_atk_type, type_name,
-					      query.class_size, class_init,
-					      query.instance_size, NULL, 0);
-}
-
-
 static GQuark
 get_quark_accessible (void)
 {
