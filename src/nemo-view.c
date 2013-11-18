e8a52016ac3bba6d9ec41926368628562aa5a0b9
Daniel Schürmann <daschuer@gmx.de>
2013-11-18 22:38:51 +0100

    Merge commit 'e76c79022d4cd8d72d7bc972f5458a87f1695d82' into nemo_master_merge
    Conflicts:
    	libnemo-private/nemo-file-utilities.c
    	libnemo-private/nemo-file-utilities.h
    	src/nemo-view.c

--- src/nemo-view.c
+++ src/nemo-view.c
@@ -2342,7 +2342,7 @@ set_up_scripts_directory_global (void)
 
 	scripts_directory_path = nemo_get_scripts_directory_path ();
 
-	if (g_mkdir_with_parents (scripts_directory_path, 0755) == 0) {
+	if (g_mkdir_with_parents (scripts_directory_path, 0700) == 0) {
 		scripts_directory_uri = g_filename_to_uri (scripts_directory_path, NULL, NULL);
 		scripts_directory_uri_length = strlen (scripts_directory_uri);
 	}
