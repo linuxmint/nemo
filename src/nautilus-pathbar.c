a7a2a9313b195c5329b5c439d2cec99c5fc23f1e
Yosef Or Boczko <yoseforb@gmail.com>
2014-01-19 14:26:56 +0200

    path bar: Request enough natural width to fit all buttons
    Otherwise, we are at the mercy of the container giving us
    more space than we request, which does not always work.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=722542

--- src/nautilus-pathbar.c
+++ src/nautilus-pathbar.c
@@ -359,7 +359,7 @@ nautilus_path_bar_get_preferred_width (GtkWidget *widget,
 		}
 
 		*minimum = MAX (*minimum, child_min);
-		*natural = MAX (*natural, child_nat);
+		*natural = *natural + child_nat;
 	}
 
 	/* Add space for slider, if we have more than one path */
