60f0a8425b0b1ff6d9d75ceb4215447587f4496f
Michael Webster <miketwebster@gmail.com>
2014-04-10 19:39:31 -0400

    Upstream fix for mintstick/nemo format woes.
    https://git.gnome.org/browse/nautilus/commit/?id=0852847e2613dbcebb4ed7f58e5b2aee3aa86a90
    https://git.gnome.org/browse/nautilus/commit/?id=a23ed4ad0173ca7a51e26282b85d06ce4b25912a

--- libnemo-private/nemo-monitor.c
+++ libnemo-private/nemo-monitor.c
@@ -33,6 +33,8 @@
 
 struct NemoMonitor {
 	GFileMonitor *monitor;
+    GVolumeMonitor *volume_monitor;
+    GFile *location;
 };
 
 gboolean
@@ -67,6 +69,33 @@ call_consume_changes_idle_cb (gpointer not_used)
 	nemo_file_changes_consume_changes (TRUE);
 	call_consume_changes_idle_id = 0;
 	return FALSE;
+}
+
+
+schedule_call_consume_changes (void)
+{
+  if (call_consume_changes_idle_id == 0) {
+      call_consume_changes_idle_id =
+          g_idle_add (call_consume_changes_idle_cb, NULL);
+  }
+}
+
+static void
+mount_removed (GVolumeMonitor *volume_monitor,
+         GMount *mount,
+         gpointer user_data)
+{
+  NemoMonitor *monitor = user_data;
+  GFile *mount_location;
+
+  mount_location = g_mount_get_root (mount);
+
+  if (g_file_has_prefix (monitor->location, mount_location)) {
+      nemo_file_changes_queue_file_removed (monitor->location);
+      schedule_call_consume_changes ();
+  }
+
+  g_object_unref (mount_location);
 }
 
 static void
@@ -111,10 +140,7 @@ dir_changed (GFileMonitor* monitor,
 	g_free (uri);
 	g_free (to_uri);
 
-	if (call_consume_changes_idle_id == 0) {
-		call_consume_changes_idle_id = 
-			g_idle_add (call_consume_changes_idle_cb, NULL);
-	}
+    schedule_call_consume_changes ();
 }
  
 NemoMonitor *
@@ -123,14 +149,26 @@ nemo_monitor_directory (GFile *location)
 	GFileMonitor *dir_monitor;
 	NemoMonitor *ret;
 
+    ret = g_new0 (NemoMonitor, 1);
 	dir_monitor = g_file_monitor_directory (location, G_FILE_MONITOR_WATCH_MOUNTS, NULL, NULL);
 
-	ret = g_new0 (NemoMonitor, 1);
-	ret->monitor = dir_monitor;
+    if (dir_monitor != NULL) {
+        ret->monitor = dir_monitor;
+    } else if (!g_file_is_native (location)) {
+        ret->location = g_object_ref (location);
+        ret->volume_monitor = g_volume_monitor_get ();
+    }
 
-	if (ret->monitor) {
-		g_signal_connect (ret->monitor, "changed", (GCallback)dir_changed, ret);
-	}
+    if (ret->monitor != NULL) {
+        g_signal_connect (ret->monitor, "changed",
+                          G_CALLBACK (dir_changed), ret);
+    }
+
+
+    if (ret->volume_monitor != NULL) {
+        g_signal_connect (ret->volume_monitor, "mount-removed",
+                          G_CALLBACK (mount_removed), ret);
+    }
 
 	/* We return a monitor even on failure, so we can avoid later trying again */
 	return ret;
@@ -145,5 +183,11 @@ nemo_monitor_cancel (NemoMonitor *monitor)
 		g_object_unref (monitor->monitor);
 	}
 
+    if (monitor->volume_monitor != NULL) {
+        g_signal_handlers_disconnect_by_func (monitor->volume_monitor, mount_removed, monitor);
+        g_object_unref (monitor->volume_monitor);
+    }
+
+    g_clear_object (&monitor->location);
 	g_free (monitor);
 }
