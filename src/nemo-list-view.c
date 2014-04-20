743cac710dd72d38c20e1695d492b1b7f19b0495
Michael Webster <miketwebster@gmail.com>
2014-04-20 14:49:24 -0400

    Improve behavior of list view columns when things get crowded, like when in dual-pane view.
    Fixes #413

--- src/nemo-list-view.c
+++ src/nemo-list-view.c
@@ -1915,12 +1915,15 @@ create_and_set_up_tree_view (NemoListView *view)
 	GList *nemo_columns;
 	GList *l;
 	gchar **default_column_order, **default_visible_columns;
-	
+
 	view->details->tree_view = GTK_TREE_VIEW (gtk_tree_view_new ());
 	view->details->columns = g_hash_table_new_full (g_str_hash, 
 							g_str_equal,
 							(GDestroyNotify) g_free,
 							(GDestroyNotify) g_object_unref);
+
+    gtk_scrollable_set_hscroll_policy (GTK_SCROLLABLE (view->details->tree_view), GTK_SCROLL_NATURAL);
+
 	gtk_tree_view_set_enable_search (view->details->tree_view, TRUE);
 
 	/* Don't handle backspace key. It's used to open the parent folder. */
@@ -2044,6 +2047,7 @@ create_and_set_up_tree_view (NemoListView *view)
 			gtk_tree_view_column_set_title (view->details->file_name_column, _("Name"));
 			gtk_tree_view_column_set_resizable (view->details->file_name_column, TRUE);
             gtk_tree_view_column_set_min_width (view->details->file_name_column, 125);
+            gtk_tree_view_column_set_sizing (view->details->file_name_column, GTK_TREE_VIEW_COLUMN_FIXED);
             gtk_tree_view_column_set_reorderable (view->details->file_name_column, TRUE);
 
             gtk_tree_view_column_set_expand (view->details->file_name_column, TRUE);
@@ -2057,8 +2061,8 @@ create_and_set_up_tree_view (NemoListView *view)
 			cell = gtk_cell_renderer_text_new ();
 			view->details->file_name_cell = (GtkCellRendererText *)cell;
             g_object_set (cell,
-                          "ellipsize", PANGO_ELLIPSIZE_END,
                           "xpad", 5,
+                          "ellipsize", PANGO_ELLIPSIZE_END,
                           NULL);
 			g_signal_connect (cell, "edited", G_CALLBACK (cell_renderer_edited), view);
 			g_signal_connect (cell, "editing-canceled", G_CALLBACK (cell_renderer_editing_canceled), view);
@@ -2080,6 +2084,8 @@ create_and_set_up_tree_view (NemoListView *view)
 									   cell,
 									   "text", column_num,
 									   NULL);
+            gtk_tree_view_column_set_sizing (column, GTK_TREE_VIEW_COLUMN_FIXED);
+            gtk_tree_view_column_set_min_width (column, 10);
             g_object_ref_sink (column);
 			gtk_tree_view_column_set_sort_column_id (column, column_num);
 			g_hash_table_insert (view->details->columns, 
@@ -2089,6 +2095,7 @@ create_and_set_up_tree_view (NemoListView *view)
 			gtk_tree_view_column_set_resizable (column, TRUE);
             gtk_tree_view_column_set_visible (column, TRUE);
             gtk_tree_view_column_set_reorderable (column, TRUE);
+            gtk_tree_view_column_set_expand (column, TRUE);
 		}
 		g_free (name);
 		g_free (label);
