38faed17aec9c2eeec4f2b3fe940ec9aabcf87ac
Daniel Schürmann <daschuer@gmx.de>
2014-05-04 20:22:05 +0200

    unity launcher support

--- src/nemo-application.c
+++ src/nemo-application.c
@@ -78,6 +78,10 @@
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 
+#ifdef HAVE_UNITY
+#include "src/unity-bookmarks-handler.h"
+#endif
+
 /* Keep window from shrinking down ridiculously small; numbers are somewhat arbitrary */
 #define APPLICATION_WINDOW_MIN_WIDTH	300
 #define APPLICATION_WINDOW_MIN_HEIGHT	100
@@ -1217,6 +1221,10 @@ nemo_application_startup (GApplication *app)
 	 */
 	check_required_directories (self);
 	init_desktop (self);
+
+#ifdef HAVE_UNITY
+	unity_bookmarks_handler_initialize ();
+#endif
 }
 
 static void
