60b3eceeae8372a136881d52ba68d6ca07ef32d1
Michael Webster <miketwebster@gmail.com>
2014-07-19 11:10:52 -0400

    Revert "Merge pull request #661 from bkabrda/master"
    This reverts commit a83e56c95210dcb703d1516b1ebd1ed4a92c0bdb, reversing
    changes made to ea1658c7216f39b3625f5f7e12ddf3ef05e0596d.

--- files/usr/share/nemo/actions/myaction.py
+++ files/usr/share/nemo/actions/myaction.py
@@ -3,11 +3,11 @@
 import sys
 
 command = sys.argv[0]
-print("Running " + command)
-print("With the following arguments:")
+print "Running " + command
+print "With the following arguments:"
 for arg in sys.argv:
     if command == arg:
         continue
     else:
-        print(arg)
+        print arg
 
