bf3608459f665e6e22fcd1989ac919d921910b0e
Daniel Schürmann <daschuer@gmx.de>
2014-11-12 20:17:32 +0100

    all: remove nemo_window_slot_go_to()
    this is based on Nautilus commit add04d84e0fa12a1fad59f0e3caa330ada37d502
    and required for the search integration
    
    Original commit message:
    "We don't want to have similar but slightly different code paths;
    consolidate use of NautilusWindowOpenFlags everywhere and simplify code."

--- src/nemo-mime-actions.c
+++ src/nemo-mime-actions.c
@@ -1536,8 +1536,7 @@ activate_files (ActivateParameters *parameters)
 
 			uri = nemo_file_get_activation_uri (file);
 			f = g_file_new_for_uri (uri);
-			nemo_window_slot_open_location (parameters->slot,
-							    f, flags, NULL);
+			nemo_window_slot_open_location (parameters->slot, f, flags);
 			g_object_unref (f);
 			g_free (uri);
 		}
