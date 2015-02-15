5fcfa3cf191e083d38699ac581ae4dcce1ccd9da
Daniel Schürmann <daschuer@gmx.de>
2015-02-15 20:45:17 +0100

    properties-window: expose ellipsis mode of attach_label()

--- src/nemo-properties-window.c
+++ src/nemo-properties-window.c
@@ -1188,19 +1188,16 @@ static GtkLabel *
 attach_label (GtkGrid *grid,
 	      GtkWidget *sibling,
 	      const char *initial_text,
-	      gboolean right_aligned,
-	      gboolean bold,
-	      gboolean ellipsize_text,
+	      PangoEllipsizeMode ellipsize_mode,
 	      gboolean selectable,
 	      gboolean mnemonic)
 {
 	GtkWidget *label_field;
 
-	if (ellipsize_text) {
+	if (ellipsize_mode != PANGO_ELLIPSIZE_NONE) {
 		label_field = gtk_label_new (initial_text);
                 gtk_label_set_ellipsize (GTK_LABEL (label_field),
-                                         right_aligned ? PANGO_ELLIPSIZE_START :
-                                                         PANGO_ELLIPSIZE_END);
+					 ellipsize_mode);
 	} else if (mnemonic) {
 		label_field = gtk_label_new_with_mnemonic (initial_text);
 	} else {
@@ -1210,14 +1207,11 @@ attach_label (GtkGrid *grid,
 	if (selectable) {
 		gtk_label_set_selectable (GTK_LABEL (label_field), TRUE);
 	}
-	
-	if (bold) {
-		eel_gtk_label_make_bold (GTK_LABEL (label_field));
-	}
-	gtk_misc_set_alignment (GTK_MISC (label_field), right_aligned ? 1 : 0, 0.5);
+
+	gtk_misc_set_alignment (GTK_MISC (label_field), 0, 0.5);
 	gtk_widget_show (label_field);
 
-	if (ellipsize_text) {
+	if (ellipsize_mode != PANGO_ELLIPSIZE_NONE) {
 		gtk_widget_set_hexpand (label_field, TRUE);
 	}
 
@@ -1236,7 +1230,7 @@ attach_value_label (GtkGrid *grid,
 		    GtkWidget *sibling,
 		    const char *initial_text)
 {
-	return attach_label (grid, sibling, initial_text, FALSE, FALSE, FALSE, TRUE, FALSE);
+	return attach_label (grid, sibling, initial_text, PANGO_ELLIPSIZE_NONE, TRUE, FALSE);
 }
 
 static GtkLabel *
@@ -1244,7 +1238,7 @@ attach_ellipsizing_value_label (GtkGrid *grid,
 				GtkWidget *sibling,
 				const char *initial_text)
 {
-	return attach_label (grid, sibling, initial_text, FALSE, FALSE, TRUE, TRUE, FALSE);
+	return attach_label (grid, sibling, initial_text, PANGO_ELLIPSIZE_END, TRUE, FALSE);
 }
 
 static GtkWidget*
@@ -2235,7 +2229,7 @@ static GtkLabel *
 attach_title_field (GtkGrid *grid,
 		    const char *title)
 {
-	return attach_label (grid, NULL, title, FALSE, FALSE, FALSE, FALSE, TRUE);
+	return attach_label (grid, NULL, title, PANGO_ELLIPSIZE_NONE, FALSE, TRUE);
 }		      
 
 #define INCONSISTENT_STATE_STRING \
