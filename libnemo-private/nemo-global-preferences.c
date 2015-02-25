200231fc09eaf243ca897c0695acef89d8596951
Daniel Schürmann <daschuer@gmx.de>
2015-02-25 21:52:38 +0100

    Merge commit '96554c2020b19c11eb065b24c9fc5457626c8bc2' into nemo_310_merge
    Conflicts:
    	libnemo-private/nemo-global-preferences.c
    	libnemo-private/nemo-global-preferences.h
    	po/es.po

--- libnemo-private/nemo-global-preferences.c
+++ libnemo-private/nemo-global-preferences.c
@@ -36,6 +36,21 @@
 
 static gboolean ignore_view_metadata = FALSE;
 
+GSettings *nemo_preferences;
+GSettings *nemo_canvas_view_preferences;
+GSettings *nemo_list_view_preferences;
+GSettings *nemo_compact_view_preferences;
+GSettings *nemo_desktop_preferences;
+GSettings *nemo_tree_sidebar_preferences;
+GSettings *nemo_window_state;
+GSettings *nemo_plugin_preferences;
+GSettings *gtk_filechooser_preferences;
+GSettings *gnome_lockdown_preferences;
+GSettings *gnome_background_preferences;
+GSettings *gnome_media_handling_preferences;
+GSettings *gnome_terminal_preferences;
+GSettings *cinnamon_privacy_preferences;
+
 /*
  * Public functions
  */
--- libnemo-private/nemo-global-preferences.h
+++ libnemo-private/nemo-global-preferences.h
@@ -250,20 +250,20 @@ gboolean nemo_global_preferences_get_ignore_view_metadata (void);
 gint nemo_global_preferences_get_tooltip_flags (void);
 gboolean nemo_global_preferences_should_load_plugin (const gchar *name, const gchar *key);
 
-GSettings *nemo_preferences;
-GSettings *nemo_canvas_view_preferences;
-GSettings *nemo_list_view_preferences;
-GSettings *nemo_compact_view_preferences;
-GSettings *nemo_desktop_preferences;
-GSettings *nemo_tree_sidebar_preferences;
-GSettings *nemo_window_state;
-GSettings *nemo_plugin_preferences;
-GSettings *gtk_filechooser_preferences;
-GSettings *gnome_lockdown_preferences;
-GSettings *gnome_background_preferences;
-GSettings *gnome_media_handling_preferences;
-GSettings *gnome_terminal_preferences;
-GSettings *cinnamon_privacy_preferences;
+extern GSettings *nemo_preferences;
+extern GSettings *nemo_canvas_view_preferences;
+extern GSettings *nemo_list_view_preferences;
+extern GSettings *nemo_compact_view_preferences;
+extern GSettings *nemo_desktop_preferences;
+extern GSettings *nemo_tree_sidebar_preferences;
+extern GSettings *nemo_window_state;
+extern GSettings *nemo_plugin_preferences;
+extern GSettings *gtk_filechooser_preferences;
+extern GSettings *gnome_lockdown_preferences;
+extern GSettings *gnome_background_preferences;
+extern GSettings *gnome_media_handling_preferences;
+extern GSettings *gnome_terminal_preferences;
+extern GSettings *cinnamon_privacy_preferences;
 
 G_END_DECLS
 
