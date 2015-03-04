b0f7d30661be222d5444817e50e93d9580cd9161
Daniel Schürmann <daschuer@gmx.de>
2015-03-04 21:34:03 +0100

    Merge commit '3a6053f1d753300f4adc3e0cffc4c6e7d0ddb52f' into nemo_310_merge
    Conflicts:
    	libnemo-private/nemo-file.c
    	po/zh_HK.po
    	po/zh_TW.po
    	src/nemo-properties-window.c

--- src/nemo-properties-window.c
+++ src/nemo-properties-window.c
@@ -423,7 +423,7 @@ static void
 update_properties_window_icon (NemoPropertiesWindow *window)
 {
 	GdkPixbuf *pixbuf;
-    cairo_surface_t *surface;
+	cairo_surface_t *surface;
 	char *name;
 
 	get_image_for_properties_window (window, &name, &pixbuf);
@@ -434,13 +434,13 @@ update_properties_window_icon (NemoPropertiesWindow *window)
 		gtk_window_set_icon (GTK_WINDOW (window), pixbuf);
 	}
 
-    surface = gdk_cairo_surface_create_from_pixbuf (pixbuf, gtk_widget_get_scale_factor (GTK_WIDGET (window)),
-                                                    gtk_widget_get_window (GTK_WIDGET (window)));
-    gtk_image_set_from_surface (GTK_IMAGE (window->details->icon_image), surface);
+	surface = gdk_cairo_surface_create_from_pixbuf (pixbuf, gtk_widget_get_scale_factor (GTK_WIDGET (window)),
+							gtk_widget_get_window (GTK_WIDGET (window)));
+	gtk_image_set_from_surface (GTK_IMAGE (window->details->icon_image), surface);
 
 	g_free (name);
 	g_object_unref (pixbuf);
-    cairo_surface_destroy (surface);
+	cairo_surface_destroy (surface);
 }
 
 /* utility to test if a uri refers to a local image */
@@ -540,11 +540,11 @@ create_image_widget (NemoPropertiesWindow *window,
 {
  	GtkWidget *button;
 	GtkWidget *image;
-
+	
 	image = gtk_image_new ();
-    window->details->icon_image = image;
+	window->details->icon_image = image;
 
-    update_properties_window_icon (window);
+	update_properties_window_icon (window);
 	gtk_widget_show (image);
 
 	button = NULL;
@@ -1069,7 +1069,6 @@ properties_window_update (NemoPropertiesWindow *window,
 	if (dirty_original) {
 		update_properties_window_title (window);
 		update_properties_window_icon (window);
-
 		update_name_field (window);
 
 		/* If any of the value fields start to depend on the original
