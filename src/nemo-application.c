71601191cdbb446785601bf4941373523b9a8b61
Daniel Schürmann <daschuer@gmx.de>
2015-03-31 21:22:19 +0200

    Merge commit 'cbc37d3ecddaf71f152bb9e7c1cf83c06e0366fc' into nemo_312_merge
    Conflicts:
    	COPYING
    	libnemo-extension/nemo-column-provider.c
    	libnemo-extension/nemo-column-provider.h
    	libnemo-extension/nemo-column.c
    	libnemo-extension/nemo-column.h
    	libnemo-extension/nemo-extension-private.h
    	libnemo-extension/nemo-extension-types.c
    	libnemo-extension/nemo-extension-types.h
    	libnemo-extension/nemo-file-info.c
    	libnemo-extension/nemo-file-info.h
    	libnemo-extension/nemo-info-provider.c
    	libnemo-extension/nemo-info-provider.h
    	libnemo-extension/nemo-location-widget-provider.c
    	libnemo-extension/nemo-location-widget-provider.h
    	libnemo-extension/nemo-menu-item.c
    	libnemo-extension/nemo-menu-item.h
    	libnemo-extension/nemo-menu-provider.c
    	libnemo-extension/nemo-menu-provider.h
    	libnemo-extension/nemo-menu.c
    	libnemo-extension/nemo-menu.h
    	libnemo-extension/nemo-property-page-provider.c
    	libnemo-extension/nemo-property-page-provider.h
    	libnemo-extension/nemo-property-page.c
    	libnemo-extension/nemo-property-page.h
    	libnemo-private/nemo-dbus-manager.c
    	libnemo-private/nemo-dbus-manager.h
    	libnemo-private/nemo-module.c
    	libnemo-private/nemo-module.h
    	libnemo-private/nemo-signaller.c
    	libnemo-private/nemo-signaller.h
    	nautilus-sendto-extension/nautilus-nste.c
    	nautilus-sendto-extension/nautilus-nste.h
    	src/nautilus-actions.h
    	src/nemo-application.c
    	src/nemo-bookmark-list.c
    	src/nemo-bookmark-list.h
    	src/nemo-bookmarks-window.c
    	src/nemo-bookmarks-window.h
    	src/nemo-desktop-item-properties.c
    	src/nemo-desktop-item-properties.h
    	src/nemo-desktop-window.c
    	src/nemo-desktop-window.h
    	src/nemo-freedesktop-dbus.c
    	src/nemo-freedesktop-dbus.h
    	src/nemo-main.c
    	src/nemo-navigation-action.c
    	src/nemo-notebook.h
    	src/nemo-places-sidebar.h
    	src/nemo-previewer.c
    	src/nemo-previewer.h
    	src/nemo-self-check-functions.c
    	src/nemo-self-check-functions.h
    	src/nemo-toolbar.h
    	src/nemo-window-menus.c
    	src/nemo-window-private.h
    	src/nemo-window.c
    	src/nemo-window.h
    	test/file-torture.py

--- src/nemo-application.c
+++ src/nemo-application.c
@@ -17,8 +17,7 @@
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Suite 500, MA 02110-1335, USA.
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  * Authors: Elliot Lee <sopwith@redhat.com>,
  *          Darin Adler <darin@bentspoon.com>
