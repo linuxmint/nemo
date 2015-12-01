f8c56c44f591ffa21db08b668bbaddf8f5a82a28
Daniel Schürmann <daschuer@gmx.de>
2015-12-01 20:31:29 +0100

    Bookmarks: don't overewrite file icon with default icon

--- libnemo-private/nemo-bookmark.c
+++ libnemo-private/nemo-bookmark.c
@@ -334,6 +334,7 @@ bookmark_file_changed_callback (NemoFile *file,
 		nemo_bookmark_disconnect_file (bookmark);
         nemo_bookmark_set_icon_to_default (bookmark);
 	} else {
+		nemo_bookmark_update_icon (bookmark);
 		bookmark_set_name_from_ready_file (bookmark, file);
 	}
 }
@@ -382,12 +383,12 @@ nemo_bookmark_connect_file (NemoBookmark *bookmark)
 		}
 	}
 
-	/* Set icon based on available information. */
-	nemo_bookmark_update_icon (bookmark);
-
-	if (bookmark->details->icon == NULL ||
-	    bookmark->details->symbolic_icon == NULL) {
-		nemo_bookmark_set_icon_to_default (bookmark);
+	if (bookmark->details->icon == NULL) {	
+		/* Set icon based on available information. */
+		nemo_bookmark_update_icon (bookmark);
+		if (bookmark->details->icon == NULL) {
+			nemo_bookmark_set_icon_to_default (bookmark);
+		}
 	}
 
 	if (bookmark->details->file != NULL &&
