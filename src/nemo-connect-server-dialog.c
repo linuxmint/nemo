86ff892ea56e5ff89bb17a19a615ee098aec0c71
Daniel Schürmann <daschuer@gmx.de>
2017-03-22 21:26:33 +0100

    connect-server-dialog: Set the arrow icon according to locale's text direction
    https://bugzilla.gnome.org/show_bug.cgi?id=727066
    https://bugzilla.gnome.org/show_bug.cgi?id=731110

--- src/nemo-connect-server-dialog.c
+++ src/nemo-connect-server-dialog.c
@@ -279,6 +279,13 @@ check_uri_entry (NemoConnectServerDialog *dialog)
 	gboolean icon_active = FALSE;
 	const char *text = NULL;
 
+#if	GTK_CHECK_VERSION(3,13,2)
+	// gtk 3.13.2 supports RTL icons in GtkIconTheme
+    const gboolean rtl = FALSE;
+#else 
+	const gboolean rtl = gtk_widget_get_direction (GTK_WIDGET (dialog)) == GTK_TEXT_DIR_RTL;
+#endif	
+
 	length = gtk_entry_get_text_length (GTK_ENTRY (dialog->details->uri_entry));
 	if (length > 0) {
 		GError *error = NULL;
@@ -297,7 +304,8 @@ check_uri_entry (NemoConnectServerDialog *dialog)
 	gtk_dialog_set_response_sensitive (GTK_DIALOG (dialog), GTK_RESPONSE_OK, button_active);
 
 	g_object_set (dialog->details->uri_entry,
-		      "secondary-icon-name", icon_active ? "edit-clear-symbolic" : NULL,
+		      "secondary-icon-name", icon_active ? (rtl ? "edit-clear-rtl-symbolic" :
+								  "edit-clear-symbolic") : NULL,
 		      "secondary-icon-activatable", icon_active,
 		      "secondary-icon-sensitive", icon_active,
 		      NULL);
