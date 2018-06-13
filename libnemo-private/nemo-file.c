dda47a5f312edb5ac70f1f93ebe32560feea0e1a
Michael Webster <miketwebster@gmail.com>
2018-06-12 21:11:16 -0400

    nemo-file.c: fix a couple pixbuf leaks, invalidate thumbnails internally when a force refresh is commanded.

--- libnemo-private/nemo-file.c
+++ libnemo-private/nemo-file.c
@@ -169,6 +169,7 @@ static const char * nemo_file_peek_display_name (NemoFile *file);
 static const char * nemo_file_peek_display_name_collation_key (NemoFile *file);
 static void file_mount_unmounted (GMount *mount,  gpointer data);
 static void metadata_hash_free (GHashTable *hash);
+static void invalidate_thumbnail (NemoFile *file);
 
 G_DEFINE_TYPE_WITH_CODE (NemoFile, nemo_file, G_TYPE_OBJECT,
 			 G_IMPLEMENT_INTERFACE (NEMO_TYPE_FILE_INFO,
@@ -454,6 +455,9 @@ nemo_file_clear_info (NemoFile *file)
 		g_object_unref (file->details->icon);
 		file->details->icon = NULL;
 	}
+
+    g_clear_object (&file->details->thumbnail);
+    g_clear_object (&file->details->scaled_thumbnail);
 
 	g_free (file->details->thumbnail_path);
 	file->details->thumbnail_path = NULL;
@@ -829,12 +833,8 @@ finalize (GObject *object)
 	g_free (file->details->activation_uri);
 	g_clear_object (&file->details->custom_icon);
 
-	if (file->details->thumbnail) {
-		g_object_unref (file->details->thumbnail);
-	}
-	if (file->details->scaled_thumbnail) {
-		g_object_unref (file->details->scaled_thumbnail);
-	}
+    g_clear_object (&file->details->thumbnail);
+    g_clear_object (&file->details->scaled_thumbnail);
 
 	if (file->details->mount) {
 		g_signal_handlers_disconnect_by_func (file->details->mount, file_mount_unmounted, file);
@@ -4207,6 +4207,7 @@ nemo_file_delete_thumbnail (NemoFile *file)
 
     gint success;
 
+    invalidate_thumbnail (file);
     success = g_unlink (file->details->thumbnail_path);
 
     if (success != 0) {
@@ -4533,7 +4534,8 @@ nemo_file_get_icon (NemoFile *file,
                 }
             }
 
-            if (file->details->thumbnail_scale == thumb_scale &&
+            if (file->details->thumbnail_is_up_to_date &&
+                file->details->thumbnail_scale == thumb_scale &&
                 file->details->scaled_thumbnail != NULL) {
                 scaled_pixbuf = file->details->scaled_thumbnail;
             } else {
