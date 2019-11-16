a352a8fd6d71e0b9944015fa6baf0b800d93e5d4
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-11-16 15:30:06 +0100

    l10n: Fix some msgids
    Use mnemonic with _..
    
    Don't use the unicode ... character (it's poorly rendered in many
    fonts)

--- src/nemo-places-sidebar.c
+++ src/nemo-places-sidebar.c
@@ -3506,7 +3506,7 @@ bookmarks_build_popup_menu (NemoPlacesSidebar *sidebar)
 	gtk_widget_show (item);
 	gtk_menu_shell_append (GTK_MENU_SHELL (sidebar->popup_menu), item);
 
-	item = gtk_menu_item_new_with_label (_("_Rename…"));
+	item = gtk_menu_item_new_with_mnemonic (_("_Rename..."));
 	sidebar->popup_menu_rename_item = item;
 	g_signal_connect (item, "activate",
 		    G_CALLBACK (rename_shortcut_cb), sidebar);
