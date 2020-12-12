e964bd4039decb9c585d39dd581aa355f1ca157d
Leigh Scott <leigh123linux@gmail.com>
2020-12-12 20:06:50 +0000

    file-operations: don't auto-escape semicolon

--- libnemo-private/nemo-file-operations.c
+++ libnemo-private/nemo-file-operations.c
@@ -3341,7 +3341,7 @@ get_max_name_length (GFile *file_dir)
 	return max_length;
 }
 
-#define FAT_FORBIDDEN_CHARACTERS "/:;*?\"<>"
+#define FAT_FORBIDDEN_CHARACTERS "/:*?\"<>\\|"
 
 static gboolean
 str_replace (char *str,
