3b6f34f63583968f472c4e1580274fdb3bbf9fa0
Michael Webster <miketwebster@gmail.com>
2024-10-11 10:14:50 -0400

    Guard against gdkx11 calls in a Wayland session.

--- src/nemo-main-application.c
+++ src/nemo-main-application.c
@@ -445,8 +445,12 @@ open_tabs_in_existing_window (NemoMainApplication *application,
 
             /* Don't use `gtk_window_present()`, as the window manager will ignore this window's focus request and try
              * to just mark it urgent instead (flashing in the window list for example). */
-            gtk_window_present_with_time (GTK_WINDOW (window),
-                                          gdk_x11_get_server_time (gtk_widget_get_window (GTK_WIDGET (window))));
+            if (eel_check_is_wayland ()) {
+                gtk_window_present (GTK_WINDOW (window));
+            } else {
+                gtk_window_present_with_time (GTK_WINDOW (window),
+                                              gdk_x11_get_server_time (gtk_widget_get_window (GTK_WIDGET (window))));
+            }
 
           break;
         }
