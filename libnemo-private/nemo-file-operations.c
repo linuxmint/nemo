a7eb0352b9cd21b377c05338d84edf300c25d5fb
a-j-bauer <a.j.bauer@t-online.de>
2026-05-10 07:14:02 +0200

    whitespace

--- libnemo-private/nemo-file-operations.c
+++ libnemo-private/nemo-file-operations.c
@@ -36,6 +36,8 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <mntent.h>
 
 #include "nemo-file-operations.h"
 
@@ -70,6 +72,7 @@
 #include "nemo-file-undo-operations.h"
 #include "nemo-file-undo-manager.h"
 #include "nemo-job-queue.h"
+#include "nemo-gfile.h"
 
 /* TODO: TESTING!!! */
 
@@ -4397,6 +4400,58 @@ get_target_file_for_display_name (GFile *dir,
 	return dest;
 }
 
+/* Determine if a given source file is a regular file e.g. no link and
+ if the target device is a mounted block device*/
+static gboolean
+gfile_src_is_regular_file_dest_is_on_block_device (GFile *src, GFile *dest)
+{
+	// 1. Check if the GFile is a regular file.
+	GFileType type =
+		g_file_query_file_type (src, G_FILE_QUERY_INFO_NONE, NULL);
+	if (type != G_FILE_TYPE_REGULAR) {
+		return FALSE;
+	}
+
+	// 2. Get the parent directory of the GFile.
+	GFile *parent = g_file_get_parent (dest);
+	if (!parent) {
+		return FALSE; // No parent (e.g., root directory).
+	}
+
+	// 3. Get the path of the parent directory.
+	gchar *parent_path = g_file_get_path (parent);
+	if (!parent_path) {
+		g_object_unref (parent);
+		return FALSE; // Parent is not a local directory.
+	}
+
+	// 4. Check if the parent directory resides on a block device.
+	FILE *mounts = setmntent ("/proc/mounts", "r");
+	if (!mounts) {
+		g_free (parent_path);
+		g_object_unref (parent);
+		return FALSE;
+	}
+
+	struct mntent *ent;
+	gboolean is_on_block_device = FALSE;
+	while ((ent = getmntent (mounts)) != NULL) {
+		if (g_str_equal (ent->mnt_dir, parent_path)) {
+			// Found the exact mount point
+			struct stat st;
+			if (stat (ent->mnt_fsname, &st) == 0) {
+				is_on_block_device = S_ISBLK (st.st_mode);
+			}
+			break;
+		}
+	}
+
+	endmntent (mounts);
+	g_free (parent_path);
+	g_object_unref (parent);
+	return is_on_block_device;
+}
+
 /* Debuting files is non-NULL only for toplevel items */
 static void
 copy_move_file (CopyMoveJob *copy_job,
@@ -4549,20 +4604,45 @@ copy_move_file (CopyMoveJob *copy_job,
 	pdata.source_info = source_info;
 	pdata.transfer_info = transfer_info;
 
-	if (copy_job->is_move) {
-		res = g_file_move (src, dest,
-				   flags,
-				   job->cancellable,
-				   copy_file_progress_callback,
-				   &pdata,
-				   &error);
+	if (gfile_src_is_regular_file_dest_is_on_block_device (src, dest)) {
+		if (copy_job->is_move) {
+			res = nemo_g_file_move_to_blk_sync (
+				src,
+				dest,
+				flags,
+				job->cancellable,
+				copy_file_progress_callback,
+				&pdata,
+				&error);
+		} else {
+			res = nemo_g_file_copy_to_blk_sync (
+				src,
+				dest,
+				flags,
+				job->cancellable,
+				copy_file_progress_callback,
+				&pdata,
+				&error);
+		}
 	} else {
-		res = g_file_copy (src, dest,
-				   flags,
-				   job->cancellable,
-				   copy_file_progress_callback,
-				   &pdata,
-				   &error);
+		// Use GIO's copy and move file operations (uses streams/pipes with splice)
+		if (copy_job->is_move) {
+			res = g_file_move (src,
+					   dest,
+					   flags,
+					   job->cancellable,
+					   copy_file_progress_callback,
+					   &pdata,
+					   &error);
+		} else {
+			res = g_file_copy (src,
+					   dest,
+					   flags,
+					   job->cancellable,
+					   copy_file_progress_callback,
+					   &pdata,
+					   &error);
+		}
 	}
 
 	if (res) {
